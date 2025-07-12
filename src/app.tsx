6fb96fdb11e21f6127386090f1d26cdd682568b9
Paranat Klinhom <paranat.klinhom@MacBook-Pro-khxng-Admin.local>
2025-07-13 00:24:42 +0700

    feat: update meta for improving seo

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,19 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
+import { Meta, MetaProvider, Title } from "@solidjs/meta";
+
 import "./app.css";
 
 export default function App() {
   return (
     <Router
-      root={props => (
-        <>
+      root={(props) => (
+        <MetaProvider>
+          <Title>List App</Title>
+          <Meta name="description" content="This is the default description for the site." />
           <Suspense>{props.children}</Suspense>
-        </>
+        </MetaProvider>
       )}
     >
       <FileRoutes />
